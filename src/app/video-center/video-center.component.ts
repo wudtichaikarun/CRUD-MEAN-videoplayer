07820f7752f2318e168f0c414782b21d5ceb6c42
wudtichaikarun <595159090004@dpu.ac.th>
2017-06-03 01:39:39 +0700

    connect to database success

---
+++ src/app/video-center/video-center.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Video } from './../video';
+import { VideoService } from './../video.service';
+
+@Component({
+  selector: 'app-video-center',
+  templateUrl: './video-center.component.html',
+  styleUrls: ['./video-center.component.css'],
+  providers: [VideoService]
+})
+export class VideoCenterComponent implements OnInit {
+
+  constructor(private _videoService: VideoService) { }
+
+
+
+  videos: Array<Video>;
+
+  ngOnInit() {
+    this._videoService.getVideos()
+    .subscribe(resVideoData => this.videos = resVideoData);
+  }
+  selectedVideo = Video;
+
+onSelectVideo(video: any){
+  this.selectedVideo = video;
+  console.log(this.selectedVideo);
+}
+
+
+}
